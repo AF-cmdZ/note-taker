3b30f1a88e411477eee1634e680b2b0adda9060b
A Fish <ashley.paz.fish@gmail.com>
2021-07-21 20:00:10 -0500

    add delete function

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,49 +1,50 @@
 import { Router } from "express";
+import fs from "fs";
+import { nanoid } from "nanoid";
 const router = new Router();
-const fs = require("fs");
-const { nanoid } = require("nanoid");
-let realPath = null;
-
-
+const realPath = null;
 
 // Get "/api/notes" responds with all notes from the database
-router.get("/notes", async (req, res) => {
-  await fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+router.get("/notes", (req, res) => {
+  fs.readFile("./app/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     return res.json(JSON.parse(data));
   });
+});
 
-router.post("/notes", ({ body }, res) => {
-  let obj = {
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  const obj = {
     id: nanoid(4),
-    title: body.title,
-    text: body.text,
+    title: req.body.title,
+    text: req.body.text,
   };
-  fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+  fs.readFile("./app/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
-    let db = JSON.parse(data);
+    const db = JSON.parse(data);
     db.push(obj);
-    fs.readFile(`${realPath}/app/db/db.json`, JSON.stringify(db), (err) => {
+    fs.writeFile("./app/db/db.json", JSON.stringify(db), (err) => {
       if (err) throw err;
-    return res.json(db);
+      return res.json(db);
+    });
   });
 });
 
 // bonus:
 // Delete "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
-  fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+  fs.readFile("./app/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     const allNotes = JSON.parse(data);
     const deleteNote = req.params.id;
 
-    const result = allNotes.filter(note => note.id != deleteNote);
+    const result = allNotes.filter((note) => note.id !== deleteNote);
 
-    fs.writeFile(`${realPath}/app/db/db.json`, JSON.stringify(result), (err) => {
-      if (err) res.json ({err: "error deleting"});
+    fs.writeFile("./app/db/db.json", JSON.stringify(result), (err) => {
+      if (err) res.json({ err: "error deleting" });
       res.json(result);
     });
   });
 });
 
-module.exports = router;
+export default router;
