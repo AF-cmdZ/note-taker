f0c372144d96e3af4bd9cd19dbff8fd465e4706f
A Fish <ashley.paz.fish@gmail.com>
2021-07-21 19:25:59 -0500

    update to routes/api.js

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,17 +1,49 @@
 import { Router } from "express";
-import db from "../db/service.js";
-
 const router = new Router();
+const fs = require("fs");
+const { nanoid } = require("nanoid");
+let realPath = null;
 
-console.log(db);
+
 
 // Get "/api/notes" responds with all notes from the database
-router.get("/notes", async (req, res) => {});
+router.get("/notes", async (req, res) => {
+  await fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+    if (err) throw err;
+    return res.json(JSON.parse(data));
+  });
 
-router.post("/notes", ({ body }, res) => {});
+router.post("/notes", ({ body }, res) => {
+  let obj = {
+    id: nanoid(4),
+    title: body.title,
+    text: body.text,
+  };
+  fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+    if (err) throw err;
+    let db = JSON.parse(data);
+    db.push(obj);
+    fs.readFile(`${realPath}/app/db/db.json`, JSON.stringify(db), (err) => {
+      if (err) throw err;
+    return res.json(db);
+  });
+});
 
 // bonus:
 // Delete "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", (req, res) => {});
+router.delete("/notes/:id", (req, res) => {
+  fs.readFile(`${realPath}/app/db/db.json`, "utf-8", (err, data) => {
+    if (err) throw err;
+    const allNotes = JSON.parse(data);
+    const deleteNote = req.params.id;
 
-export default router;
+    const result = allNotes.filter(note => note.id != deleteNote);
+
+    fs.writeFile(`${realPath}/app/db/db.json`, JSON.stringify(result), (err) => {
+      if (err) res.json ({err: "error deleting"});
+      res.json(result);
+    });
+  });
+});
+
+module.exports = router;
